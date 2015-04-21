0b99a8011cff133b796af18bec336fb4c8bd1ce9
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-21 10:43:44 +0100

    Refactors test to DRY out

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,19 +1,19 @@
 require 'bike'
 
 describe Bike do
+
+  let(:bike) { Bike.new }
+
   it 'should not be broken after we create it' do
-    bike = Bike.new
     expect(bike).not_to be_broken
   end
 
   it 'should be able to break a bike' do
-    bike = Bike.new
     bike.break!
     expect(bike).to be_broken
   end
 
   it 'should be able to be fixed when broken' do
-    bike = Bike.new
     bike.break!
     bike.fix!
     expect(bike).not_to be_broken
